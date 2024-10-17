9601517768e732aa363d7fc9076300130370f12e
nsimat <nsimba_matondo@hotmail.com>
2024-10-17 23:37:42 +0200

    Configuring the web app

--- BethanysPieShopNet8/Program.cs
+++ BethanysPieShopNet8/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews(); // For MVC configuration
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
 
 app.Run();
