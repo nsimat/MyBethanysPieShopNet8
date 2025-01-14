0b39b82a24fb885286f2864d77b1e6e10b2cf157
nsimat <nsimba_matondo@hotmail.com>
2025-01-14 14:56:25 +0100

    Adding the IPieRepository

---
+++ BethanysPieShopNet8/Models/IPieRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BethanysPieShopNet8.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int pieId);
+    }
+}
