eddb510cbb519f2a1f30302fbd6d571bb69930cf
nsimat <nsimba_matondo@hotmail.com>
2025-01-15 02:44:36 +0100

    Adding the MockPieRepository

--- BethanysPieShopNet8/Models/Pie.cs
+++ BethanysPieShopNet8/Models/Pie.cs
@@ -8,7 +8,7 @@
         public string? LongDescription { get; set; }
         public string? AllergyInformation { get; set; }
         public decimal Price { get; set; }
-        public string? imageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public string? ImageThumbnailUrl { get; set; }
         public bool IsPieOfTheWeek { get; set; }
         public bool InStock { get; set; }
