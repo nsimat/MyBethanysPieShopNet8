eb9142572d5e2c2c4589512e1ed819a1e49cab30
nsimat <nsimba_matondo@hotmail.com>
2025-01-21 01:10:38 +0100

    Configuring dependency injection to use respositories interfaces

--- BethanysPieShopNet8/Program.cs
+++ BethanysPieShopNet8/Program.cs
@@ -1,6 +1,11 @@
+using BethanysPieShopNet8.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
+builder.Services.AddScoped<IPieRepository, MockPieRepository>(); // For dependency injection
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>(); // For dependency injection
+
 builder.Services.AddControllersWithViews(); // For MVC configuration
 
 
