4f88219a1787e1cbd5b8c2f7f178167fc050d041
nsimat <nsimba_matondo@hotmail.com>
2025-01-21 01:09:19 +0100

    Adding the PieController

---
+++ BethanysPieShopNet8/Controllers/PieController.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieShopNet8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopNet8.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public IActionResult List()
+        {
+            ViewBag.Message = "Welcome to Bethanys Pie Shop";
+            var pies = _pieRepository.AllPies;
+            return View(pies);
+        }
+    }
+}
