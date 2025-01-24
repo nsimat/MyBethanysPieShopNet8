5138e191f2c272435449fe9ba637a963fb87ad42
nsimat <nsimba_matondo@hotmail.com>
2025-01-24 18:12:22 +0100

    Adding PieListViewModel for List view

---
+++ BethanysPieShopNet8/ViewModels/PieListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopNet8.Models;
+
+namespace BethanysPieShopNet8.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string? CurrentCategory { get; }
+
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCategory)
+        {
+            Pies = pies;
+            CurrentCategory = currentCategory;
+        }
+    }
+}
