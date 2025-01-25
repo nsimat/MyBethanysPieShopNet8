f6513e59793ad328dd8f508c59e1af178e0ff2c0
nsimat <nsimba_matondo@hotmail.com>
2025-01-25 21:03:21 +0100

    Adding DbContext class for Entity Framework use

---
+++ BethanysPieShopNet8/Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShopNet8.Models
+{
+    public class BethanysPieShopDbContext : DbContext
+    {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext>
+            options) : base(options)
+        {
+        }
+
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
