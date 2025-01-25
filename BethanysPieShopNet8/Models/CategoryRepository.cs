c85d3ff1768f2d298fbf527169fb353f28f80f94
nsimat <nsimba_matondo@hotmail.com>
2025-01-25 21:48:09 +0100

    Refactoring AllCategories method

---
+++ BethanysPieShopNet8/Models/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BethanysPieShopNet8.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+        public CategoryRepository(BethanysPieShopDbContext bethanysPieShopDbContext)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext;
+        }
+        public IEnumerable<Category> AllCategories => _bethanysPieShopDbContext.Categories.OrderBy(c => c.CategoryName);
+    }
+}
