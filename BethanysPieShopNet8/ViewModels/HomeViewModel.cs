ab4cac85e00657c1f76452809a5ce3516777cb33
nsimat <nsimba_matondo@hotmail.com>
2025-01-28 04:10:26 +0100

    Adding the HomeViewModel

---
+++ BethanysPieShopNet8/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShopNet8.Models;
+
+namespace BethanysPieShopNet8.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek ?? throw new ArgumentNullException(nameof(piesOfTheWeek));
+        }
+    }
+}
