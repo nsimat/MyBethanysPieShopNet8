f565b21ed96730f44745ad3b7421f31c276f81a8
nsimat <nsimba_matondo@hotmail.com>
2025-02-07 15:42:21 +0100

    Adding the HomeController

---
+++ BethanysPieShopNet8/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopNet8.Models;
+using BethanysPieShopNet8.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopNet8.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository ?? throw new ArgumentNullException(nameof(pieRepository));
+        }
+        public IActionResult Index()
+        {
+            var piesOfTheWeek = _pieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModel(piesOfTheWeek);
+            return View(homeViewModel);
+        }
+    }
+}
