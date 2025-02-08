cd57a2dc3d7f19ec7e84c9578bf4729b15e992a6
nsimat <nsimba_matondo@hotmail.com>
2025-02-08 16:17:57 +0100

    Adding _PieCard partial view

---
+++ BethanysPieShopNet8/Views/Shared/_PieCard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShopNet8.Views.Shared
+{
+    public class _PieCardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
