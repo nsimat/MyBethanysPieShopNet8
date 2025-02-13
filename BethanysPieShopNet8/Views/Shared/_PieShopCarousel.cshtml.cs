5da6f3313707d70be08a1bd1284bb41fe3277dc6
nsimat <nsimba_matondo@hotmail.com>
2025-02-13 20:48:07 +0100

    Adding a carousel partial view to the _LayOut partial view

---
+++ BethanysPieShopNet8/Views/Shared/_PieShopCarousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShopNet8.Views.Shared
+{
+    public class _PieShopCarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
