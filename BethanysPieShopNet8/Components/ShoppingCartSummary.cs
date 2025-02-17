e7c99b17ad0d113cd243ee4307d91b83c19de53d
nsimat <nsimba_matondo@yahoo.fr>
2025-02-17 19:30:06 +0100

    Adding the ShoppingCartSummary view component

---
+++ BethanysPieShopNet8/Components/ShoppingCartSummary.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShopNet8.Models;
+using BethanysPieShopNet8.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopNet8.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(IShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart ?? throw new ArgumentNullException(nameof(shoppingCart));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart,
+                _shoppingCart.GetShoppingCartTotal());
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
