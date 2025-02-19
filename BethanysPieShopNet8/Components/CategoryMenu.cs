ad25d19be83684935f6dda24c200ab74309ffe5b
nsimat <nsimba_matondo@yahoo.fr>
2025-02-19 03:35:22 +0100

    Adding CategoryMenu view component

---
+++ BethanysPieShopNet8/Components/CategoryMenu.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopNet8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopNet8.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.AllCategories.OrderBy(c => c.CategoryName);
+
+            return View(categories);
+        }
+    }
+}
