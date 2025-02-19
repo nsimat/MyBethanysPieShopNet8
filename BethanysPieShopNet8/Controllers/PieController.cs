59defba38785c37119eebc76c3b2b1c542f4d0d3
nsimat <nsimba_matondo@yahoo.fr>
2025-02-19 03:34:31 +0100

    Modifying the List method in PieController to integrate pie categories

--- BethanysPieShopNet8/Controllers/PieController.cs
+++ BethanysPieShopNet8/Controllers/PieController.cs
@@ -16,11 +16,26 @@ namespace BethanysPieShopNet8.Controllers
         }
 
 
-        public IActionResult List()
+        public IActionResult List(string category)
         {
-            var pies = _pieRepository.AllPies;
-            PieListViewModel pieListViewModel = new PieListViewModel(pies, "All Pies");
-            return View(pieListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+
+                currentCategory = _categoryRepository.AllCategories
+                    .FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
