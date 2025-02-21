6a323a7de1e9b4c3ac59b9cf4c5e5fdc0a69dc83
nsimat <nsimba_matondo@yahoo.fr>
2025-02-21 15:36:25 +0100

    Implementing the IOrderRepository interface

---
+++ BethanysPieShopNet8/Models/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿namespace BethanysPieShopNet8.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly BethanysPieShopDbContext _bethanysPieShopDbContext;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderRepository(BethanysPieShopDbContext bethanysPieShopDbContext, ShoppingCart shoppingCart)
+        {
+            _bethanysPieShopDbContext = bethanysPieShopDbContext ?? throw new ArgumentNullException(nameof(bethanysPieShopDbContext));
+            _shoppingCart = shoppingCart ?? throw new ArgumentNullException(nameof(shoppingCart));
+        }
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+
+            List<ShoppingCartItem>? shoppingCartItems = _shoppingCart.ShoppingCartItems;
+            order.OrderTotal = _shoppingCart.GetShoppingCartTotal();
+
+            order.OrderDetails = new List<OrderDetail>();
+
+            // Adding an order with its details
+
+            foreach (ShoppingCartItem? shoppingCartItem in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail
+                {
+                    Amount = shoppingCartItem.Amount,
+                    PieId = shoppingCartItem.Pie.PieId,
+                    Price = shoppingCartItem.Pie.Price
+                };
+
+                order.OrderDetails.Add(orderDetail);
+            }
+
+            _bethanysPieShopDbContext.Orders.Add(order);
+
+            _bethanysPieShopDbContext.SaveChanges();
+        }
+    }
+}
