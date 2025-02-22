2deae34791756c61bf818f1a0eb022df1c0bad25
nsimat <nsimba_matondo@yahoo.fr>
2025-02-22 02:36:35 +0100

    Adding the OrderController

---
+++ BethanysPieShopNet8/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopNet8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopNet8.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
