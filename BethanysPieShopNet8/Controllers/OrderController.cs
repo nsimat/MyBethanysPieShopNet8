5e17663d2ff4a7b144c554423ecb9bf83a4b7591
nsimat <nsimba_matondo@yahoo.fr>
2025-02-22 22:00:36 +0100

    Adding the Checkout process of an order

--- BethanysPieShopNet8/Controllers/OrderController.cs
+++ BethanysPieShopNet8/Controllers/OrderController.cs
@@ -18,5 +18,26 @@ namespace BethanysPieShopNet8.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
     }
 }
