78dfca10f038f8689d1cee1ec7c3215d49187f68
nsimat <nsimba_matondo@yahoo.fr>
2025-03-08 03:03:50 +0100

    Adding the Checkout razor page

---
+++ BethanysPieShopNet8/Pages/CheckoutPage.cshtml.cs
@@ -0,0 +1,49 @@
+using BethanysPieShopNet8.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShopNet8.Pages
+{
+    public class CheckoutPageModel : PageModel
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public CheckoutPageModel(IOrderRepository orderRepository, IShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public Order Order { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(Order);
+                _shoppingCart.ClearCart();
+                return RedirectToPage("CheckoutComplete");
+            }
+
+            return Page();
+        }
+    }
+}
