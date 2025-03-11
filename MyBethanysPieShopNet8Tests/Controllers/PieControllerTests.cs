85261b418c83626dca8475dc96375682eee51356
nsimat <nsimba_matondo@yahoo.fr>
2025-03-11 23:52:14 +0100

    Adding first PieController test

---
+++ MyBethanysPieShopNet8Tests/Controllers/PieControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using BethanysPieShopNet8.Controllers;
+using BethanysPieShopNet8.ViewModels;
+using BethanysPieShopNet8Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopNet8Tests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void list_EmptyCategory_ReturnsAllPies()
+        {
+            // Arrange
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            // Act
+            var result = pieController.List("");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
