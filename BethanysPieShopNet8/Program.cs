394bbeaf8f34d203a47f2eedc71053d14ce37f24
nsimat <nsimba_matondo@yahoo.fr>
2025-08-11 23:22:47 +0200

    Adding secrets.json to the configuration database connection

--- BethanysPieShopNet8/Program.cs
+++ BethanysPieShopNet8/Program.cs
@@ -1,4 +1,4 @@
-using BethanysPieShopNet8.Models;
+﻿using BethanysPieShopNet8.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,20 +7,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IPieRepository, PieRepository>(); // For dependency injection
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); // For dependency injection
 builder.Services.AddScoped<IOrderRepository, OrderRepository>(); // For dependency injection
-
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp)); // For dependency injection
+
 builder.Services.AddSession(); // For session state
+
 builder.Services.AddHttpContextAccessor(); // For session state
 
 builder.Services.AddControllersWithViews(); // For MVC configuration
-builder.Services.AddRazorPages(); // For Razor Pages configuration
-builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 
+builder.Services.AddRazorPages(); // For Razor Pages configuration
+
+// Add secrets.json to the configuration and database connection
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
  options.UseSqlServer(builder.Configuration.GetConnectionString("BethanysPieShopDbContext")));
+
+builder.Services.AddControllers(); // For API configuration
 
 var app = builder.Build();
 
 app.UseStaticFiles();// For static files
+
 app.UseSession(); // For session state
 
 if (app.Environment.IsDevelopment())
@@ -29,12 +37,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute(); // "{controller=Home}/{action=Index}/{id?}"
-                                 //app.MapControllerRoute(
-                                 //name: "default",
-                                 //pattern: "{controller=Home}/{action=Index}/{id?}");
+                                 // app.MapControllerRoute(
+                                 // name: "default",
+                                 // pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages(); // For Razor Pages
 
 DbInitializer.Seed(app); // Seed the database
 
-app.Run();
+await app.RunAsync();
